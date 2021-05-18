f4c6c0c1fd50b235b9b9a462d2483768ad3633fb
cytoncode <cytoncode@gmail.com>
2021-05-18 18:09:53 +0300

    Убраны файлы компиляции и запуска программы для с++ программ

--- echo/Linux_compile.py
+++
@@ -1,8 +0,0 @@
-import os
-
-source_file = "main.cpp"  # link to source code
-app_link = "bin/echo"  # link where the compiled program should be
-cmd = "clang -o {} {}".format(app_link, source_file)  # ready command
-#  line
-
-os.system(cmd)  # compiling the file
